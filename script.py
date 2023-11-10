42b386c5ba77ce4b12452bb66383bf00c4646120
marcho <madergilev@edu.hse.ru>
2023-11-11 00:58:30 +0300

    oop

--- script.py
+++ script.py
@@ -12,41 +12,45 @@ with open("proxies.txt", "r") as f:
         proxies.append(x)
 
 
-# Function to send message from user
-def send_message(tkn: str, channel_id: str, msg: str, proxy=""):
-    headers = {"accept": "*/*",
-               "accept-encoding": "gzip, deflate",
-               "accept-language": "en-US",
-               "authorization": tkn,
-               "dnt": "1",
-               "referer": "https://discord.com/channels/@me",
-               "sec-ch-ua-mobile": "?0",
-               "sec-ch-ua-platform": "\"Windows\"",
-               "sec-fetch-dest": "empty",
-               "sec-fetch-mode": "cors",
-               "Content-Type": "application/json",
-               "sec-fetch-site": "same-origin",
-               "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
-                             "Chrome/108.0.0.0 Safari/537.36",
-               "x-debug-options": "bugReporterEnabled",
-               "x-discord-locale": "en-US"}
-    content = {"content": msg}
-    url = f"https://discord.com/api/v9/channels/{str(channel_id)}/messages"
-    if proxy == "":
-        response = requests.post(url=url,
-                                 headers=headers,
-                                 json=content)
-        answer = response.json()
-        print(answer["author"]["username"] + " sent message " + answer["content"] + " successfully")
-    else:
-        p = {'http': proxy,
-             "https": proxy}
-        response = requests.post(url=url,
-                                 headers=headers,
-                                 json=content,
-                                 proxies=p)
-        answer = response.json()
-        print(answer["author"]["username"] + " sent message " + answer["content"] + " successfully")
+class User:
+    def __init__(self, token):
+        self._token = token
+        self._headers = {"accept": "*/*",
+                   "accept-encoding": "gzip, deflate",
+                   "accept-language": "en-US",
+                   "authorization": self._token,
+                   "dnt": "1",
+                   "referer": "https://discord.com/channels/@me",
+                   "sec-ch-ua-mobile": "?0",
+                   "sec-ch-ua-platform": "\"Windows\"",
+                   "sec-fetch-dest": "empty",
+                   "sec-fetch-mode": "cors",
+                   "Content-Type": "application/json",
+                   "sec-fetch-site": "same-origin",
+                   "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                                 "Chrome/108.0.0.0 Safari/537.36",
+                   "x-debug-options": "bugReporterEnabled",
+                   "x-discord-locale": "en-US"}
+
+    def send_message(self, channel_id: str, msg: str, proxy=""):
+        headers = self._headers
+        content = {"content": msg}
+        url = f"https://discord.com/api/v9/channels/{str(channel_id)}/messages"
+        if proxy == "":
+            response = requests.post(url=url,
+                                     headers=headers,
+                                     json=content)
+            answer = response.json()
+            print(answer["author"]["username"] + " sent message " + answer["content"] + " successfully")
+        else:
+            p = {'http': proxy,
+                 "https": proxy}
+            response = requests.post(url=url,
+                                     headers=headers,
+                                     json=content,
+                                     proxies=p)
+            answer = response.json()
+            print(answer["author"]["username"] + " sent message " + answer["content"] + " successfully")
 
 
 channelId = "1060736059215970345"  # channel id
@@ -57,7 +61,7 @@ delay = 3605  # delay in seconds
 def main():
     while True:
         for i in range(len(tokens)):
-            send_message(tokens[i], channelId, "!work", proxy=proxies[i % len(proxies) - 1])
+            User(tokens[i]).send_message(channelId, "!work", proxy=proxies[i % len(proxies) - 1])
         time.sleep(delay)
 
 
